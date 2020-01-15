6aadd278eff678a80fdab3ad2a30a64b8255be87
bio8oid <8bollod8@mail.co.uk>
2020-01-15 01:15:30 +0100

    Update home component text

--- src/presentational/home.component.js
+++ src/presentational/home.component.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => (
     <div>
         <h1>Hello!</h1>
-        <h1>This is Countries.js App</h1>
+        <h1>This is React based App.</h1>
+        <h1>Also Redux and React-router has been used ;)</h1>
     </div>
 );
 
